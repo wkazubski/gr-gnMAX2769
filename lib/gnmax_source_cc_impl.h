977f81cab1fedbeaea306e6d91ad27ccd3c0b6bd
Wojciech Kazubski <wk@dracena.ire.pw.edu.pl>
2020-09-29 14:43:34 +0200

    Port to GNU Radio 3.8

--- lib/gnmax_source_cc_impl.h
+++ lib/gnmax_source_cc_impl.h
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/* 
- * Copyright 2019 Wojciech Kazubski.
- * 
+/*
+ * Copyright 2020 Wojciech Kazubski.
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -22,6 +22,7 @@
 #define INCLUDED_GNMAX2769_GNMAX_SOURCE_CC_IMPL_H
 
 #include <gnMAX2769/gnmax_source_cc.h>
+#include "gnmax_source.h"
 
 namespace gr {
   namespace gnMAX2769 {
@@ -39,9 +40,11 @@ namespace gr {
       ~gnmax_source_cc_impl();
 
       // Where all the action really happens
-      int work(int noutput_items,
+      int work(
+              int noutput_items,
               gr_vector_const_void_star &input_items,
-              gr_vector_void_star &output_items);
+              gr_vector_void_star &output_items
+      );
 
       int bias() const { return settings.bias; };
       void set_bias (int bias);
@@ -57,3 +60,4 @@ namespace gr {
 } // namespace gr
 
 #endif /* INCLUDED_GNMAX2769_GNMAX_SOURCE_CC_IMPL_H */
+
--- swig/gnMAX2769_swig.i
+++ swig/gnMAX2769_swig.i
@@ -2,7 +2,7 @@
 
 #define GNMAX2769_API
 
-%include "gnuradio.i"			// the common stuff
+%include "gnuradio.i"           // the common stuff
 
 //load generated python docstrings
 %include "gnMAX2769_swig_doc.i"
@@ -10,7 +10,6 @@
 %{
 #include "gnMAX2769/gnmax_source_cc.h"
 %}
-
 
 %include "gnMAX2769/gnmax_source_cc.h"
 GR_SWIG_BLOCK_MAGIC2(gnMAX2769, gnmax_source_cc);
